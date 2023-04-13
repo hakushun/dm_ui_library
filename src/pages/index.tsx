21b10e55fc168e7dd0df256dff10cae573546c2c
hakushun <hakushun.pianist@gmail.com>
2023-04-13 16:47:58 +0900

    feat: add nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/ui1">ui1</Link>
+          </li>
+          <li>
+            <Link href="/ui2">ui2</Link>
+          </li>
+          <li>
+            <Link href="/ui3">ui3</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
