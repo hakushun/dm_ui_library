53af6e11ab82df60726f9e76905b19119bb66736
hakushun <hakushun.pianist@gmail.com>
2023-04-13 16:41:40 +0900

    chore: set up Chakra UI

---
+++ src/components/Chakra/index.tsx
@@ -0,0 +1,3 @@
+export function Chakra() {
+  return <div>Chakra</div>;
+}
---
+++ src/pages/ui1.tsx
@@ -0,0 +1,9 @@
+import { Chakra } from "@/components/Chakra";
+
+export default function Ui1() {
+  return (
+    <main>
+      <Chakra />
+    </main>
+  );
+}
