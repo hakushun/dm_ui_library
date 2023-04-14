90a4bf91b21fa5b5a29250e014ccc8f7d4219eaa
hakushun <hakushun.pianist@gmail.com>
2023-04-14 21:48:41 +0900

    feat: add table component

--- src/components/Chakra/index.tsx
+++ src/components/Chakra/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   Card,
   CardBody,
   Container,
@@ -7,7 +8,15 @@ import {
   Image,
   Stack,
   StackDivider,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
   VStack,
 } from "@chakra-ui/react";
 
@@ -63,6 +72,95 @@ export function Chakra() {
             </CardBody>
           </Card>
         </HStack>
+      </Container>
+      <Container maxW="none">
+        <Heading as="h1">Table</Heading>
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th isNumeric>No.</Th>
+                <Th>公開設定</Th>
+                <Th>ソリューション名</Th>
+                <Th>型番</Th>
+                <Th>作成日</Th>
+                <Th>更新日</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td isNumeric>1</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション1</Td>
+                <Td>RSD_00001</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+              <Tr>
+                <Td isNumeric>2</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション2</Td>
+                <Td>RSD_00002</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+              <Tr>
+                <Td isNumeric>3</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション3</Td>
+                <Td>RSD_00003</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+              <Tr>
+                <Td isNumeric>4</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション4</Td>
+                <Td>RSD_00004</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+              <Tr>
+                <Td isNumeric>5</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション5</Td>
+                <Td>RSD_00005</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+              <Tr>
+                <Td isNumeric>6</Td>
+                <Td>
+                  <Badge colorScheme="green">公開中</Badge>
+                </Td>
+                <Td>ソリューション6</Td>
+                <Td>RSD_00006</Td>
+                <Td>2023/01/01 00:00:00</Td>
+                <Td>2023/01/01 00:00:00</Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th isNumeric>No.</Th>
+                <Th>公開設定</Th>
+                <Th>ソリューション名</Th>
+                <Th>型番</Th>
+                <Th>作成日</Th>
+                <Th>更新日</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
       </Container>
     </VStack>
   );
