fc3554d1c009b35de33627b3624800bc38884c6a
hakushun <hakushun.pianist@gmail.com>
2023-04-14 21:36:33 +0900

    feat: add card component

--- src/components/Chakra/index.tsx
+++ src/components/Chakra/index.tsx
@@ -1,9 +1,69 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Stack,
+  StackDivider,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 export function Chakra() {
   return (
-    <div>
-      <Button>Button</Button>
-    </div>
+    <VStack divider={<StackDivider borderColor="gray.200" />} spacing={4} bgColor="gray.50">
+      <Container maxW="none">
+        <Heading as="h1">Card</Heading>
+        <HStack spacing={4}>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">ソリューション名</Heading>
+                <Text fontSize="sm" textAlign="right">
+                  企業名
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">ソリューション名</Heading>
+                <Text fontSize="sm" textAlign="right">
+                  企業名
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">ソリューション名</Heading>
+                <Text fontSize="sm" textAlign="right">
+                  企業名
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        </HStack>
+      </Container>
+    </VStack>
   );
 }
