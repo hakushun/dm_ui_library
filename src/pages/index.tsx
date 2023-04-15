b69a47d71141f30a2f5ececbdde8aaedcf4a580f
hakushun <hakushun.pianist@gmail.com>
2023-04-15 11:01:47 +0900

    feat: navigationのデザイン修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
+import { Box, Link, List, ListItem, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/ui1">ui1</Link>
-          </li>
-          <li>
-            <Link href="/ui2">ui2</Link>
-          </li>
-          <li>
-            <Link href="/ui3">ui3</Link>
-          </li>
-        </ul>
-      </nav>
+      <Box maxW={300} padding={4}>
+        <VStack as="nav">
+          <List spacing={3}>
+            <ListItem>
+              <NextLink href="/ui1" passHref>
+                <Link color="blue.600" textDecoration="underline">
+                  design1
+                </Link>
+              </NextLink>
+            </ListItem>
+            <ListItem>
+              <NextLink href="/ui2" passHref>
+                <Link color="blue.600" textDecoration="underline">
+                  design2
+                </Link>
+              </NextLink>
+            </ListItem>
+            <ListItem>
+              <NextLink href="/ui3" passHref>
+                <Link color="blue.600" textDecoration="underline">
+                  design3
+                </Link>
+              </NextLink>
+            </ListItem>
+          </List>
+        </VStack>
+      </Box>
     </main>
   );
 }
