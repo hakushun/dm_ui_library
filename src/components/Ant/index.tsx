959008f318020d278548a77cf50bcb8e126b226e
ramfeather <ramfeather.show@gmail.com>
2023-04-18 23:09:15 +0900

    feat: とりあえずカード

--- src/components/Ant/index.tsx
+++ src/components/Ant/index.tsx
@@ -1,9 +1,19 @@
-import { Button } from "antd";
+import { Card, Image } from "antd";
 
 export function Ant() {
+  const { Meta } = Card;
+
   return (
-    <div>
-      <Button>Button</Button>
-    </div>
+    <Card
+      style={{ width: 300 }}
+      cover={
+        <Image
+          src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="Green double couch with wooden legs"
+        />
+      }
+    >
+      <Meta title="ソリューション名" description="企業名" />
+    </Card>
   );
 }
