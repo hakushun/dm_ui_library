30802e93d886baacd166110ed1e02656261b555c
ramfeather <ramfeather.show@gmail.com>
2023-04-19 00:51:17 +0900

    feat: 一旦カード終わり

--- src/components/Ant/index.tsx
+++ src/components/Ant/index.tsx
@@ -1,49 +1,120 @@
-import { Card, Image, Layout, Space, Typography } from "antd";
+import {
+  Card,
+  Image,
+  Layout,
+  Row,
+  Col,
+  Divider,
+  Typography,
+  theme,
+} from "antd";
 
 const { Meta } = Card;
 const { Content } = Layout;
 const { Title } = Typography;
+const { useToken } = theme;
 
 export function Ant() {
+  const { token } = useToken();
+
   return (
-    <Space direction="vertical">
-      <Space size={"large"}>
-        <Card
-          style={{ width: 300 }}
-          cover={
-            <Image
-              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="Green double couch with wooden legs"
-            />
-          }
-        >
-          <Meta title="ソリューション名" description="企業名" />
-        </Card>
-        <Card
-          style={{ width: 300 }}
-          cover={
-            <Image
-              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="Green double couch with wooden legs"
-            />
-          }
-        >
-          <Meta title="ソリューション名" description="企業名" />
-        </Card>
-        <Card
-          style={{ width: 300 }}
-          cover={
-            <Image
-              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt="Green double couch with wooden legs"
-            />
-          }
-        >
-          <Meta title="ソリューション名" description="企業名" />
-        </Card>
-      </Space>
-      {/* /Card Space */}
-    </Space>
-    // /Page Space
+    <Layout>
+      <Content
+        style={{
+          maxWidth: "none",
+          padding: "16px 24px",
+        }}
+      >
+        <Title>Card</Title>
+        <Row justify="center" gutter={[24, 16]}>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+      {/* /Card Content */}
+      <Divider style={{ margin: token.marginXS }} />
+      <Content style={{ maxWidth: "none", padding: "16px 24px" }}>
+        <Title>Table</Title>
+        <Row justify="center" gutter={[24, 16]}>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{ width: 360 }}
+              cover={
+                <Image
+                  src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="Green double couch with wooden legs"
+                />
+              }
+            >
+              <Meta title="ソリューション名" description="企業名" />
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+      {/* /Table Content */}
+    </Layout>
   );
 }
