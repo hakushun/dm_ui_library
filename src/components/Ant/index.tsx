4fa5a98f47936221b1d228edaf39315049323458
ramfeather <ramfeather.show@gmail.com>
2023-04-18 23:44:05 +0900

    feat: カードをflexに入れる

--- src/components/Ant/index.tsx
+++ src/components/Ant/index.tsx
@@ -1,19 +1,49 @@
-import { Card, Image } from "antd";
+import { Card, Image, Layout, Space, Typography } from "antd";
+
+const { Meta } = Card;
+const { Content } = Layout;
+const { Title } = Typography;
 
 export function Ant() {
-  const { Meta } = Card;
-
   return (
-    <Card
-      style={{ width: 300 }}
-      cover={
-        <Image
-          src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt="Green double couch with wooden legs"
-        />
-      }
-    >
-      <Meta title="ソリューション名" description="企業名" />
-    </Card>
+    <Space direction="vertical">
+      <Space size={"large"}>
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <Image
+              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="Green double couch with wooden legs"
+            />
+          }
+        >
+          <Meta title="ソリューション名" description="企業名" />
+        </Card>
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <Image
+              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="Green double couch with wooden legs"
+            />
+          }
+        >
+          <Meta title="ソリューション名" description="企業名" />
+        </Card>
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <Image
+              src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="Green double couch with wooden legs"
+            />
+          }
+        >
+          <Meta title="ソリューション名" description="企業名" />
+        </Card>
+      </Space>
+      {/* /Card Space */}
+    </Space>
+    // /Page Space
   );
 }
