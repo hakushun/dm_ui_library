b046dbf240f70d52edfadca386d0d6b183dcde16
Shun Nakano <s.nakano@ksj.co.jp>
2023-04-21 09:38:27 +0900

    feat: add card component

--- src/components/Mantine/index.tsx
+++ src/components/Mantine/index.tsx
@@ -1,9 +1,70 @@
-import { Button } from "@mantine/core";
+import { Card, Container, Flex, Group, Image, Stack, Text, Title } from "@mantine/core";
 
 export function Mantine() {
   return (
-    <div>
-      <Button>Button</Button>
-    </div>
+    <Stack>
+      <Container>
+        <Title order={1}>Card</Title>
+        <Flex gap="md">
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Norway"
+                width={300}
+              />
+            </Card.Section>
+            <Container mt={10} px={0}>
+              <Group>
+                <Text weight={600}>ソリューション名</Text>
+              </Group>
+              <Group position="right">
+                <Text size="sm" color="dimmed">
+                  企業名
+                </Text>
+              </Group>
+            </Container>
+          </Card>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Norway"
+                width={300}
+              />
+            </Card.Section>
+            <Container mt={10} px={0}>
+              <Group>
+                <Text weight={600}>ソリューション名</Text>
+              </Group>
+              <Group position="right">
+                <Text size="sm" color="dimmed">
+                  企業名
+                </Text>
+              </Group>
+            </Container>
+          </Card>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                src="https://images.pexels.com/photos/2599244/pexels-photo-2599244.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Norway"
+                width={300}
+              />
+            </Card.Section>
+            <Container mt={10} px={0}>
+              <Group>
+                <Text weight={600}>ソリューション名</Text>
+              </Group>
+              <Group position="right">
+                <Text size="sm" color="dimmed">
+                  企業名
+                </Text>
+              </Group>
+            </Container>
+          </Card>
+        </Flex>
+      </Container>
+    </Stack>
   );
 }
